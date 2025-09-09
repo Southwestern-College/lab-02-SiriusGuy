73eb93f871e21d8064d4c13ecf481a2fafaaef37
SiriusGuy <cs.romangodinez@gmail.com>
2025-09-09 20:09:46 +0000

    Maximum.java

---
+++ Maximum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Maximum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter variable");
+        int number1 = input.nextInt();
+
+        System.out.println("Enter variable");
+        int number2 = input.nextInt();
+
+        System.out.println("Enter variable");
+        int number3 = input.nextInt();
+
+        System.out.println("The mas is");
+        if (number1 >= number2 + number3)
+            System.out.println(number1);
+        if (number2 >= number1 + number3)
+            System.out.println(number2);
+        if (number3 >=)
+
+    }
+}
+
