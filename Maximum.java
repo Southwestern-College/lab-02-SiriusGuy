f3daa94b16ba4587370e0066bb36a0cb6edb97b0
SiriusGuy <cs.romangodinez@gmail.com>
2025-09-10 04:36:17 +0000

    Maximum.java

--- Maximum.java
+++ Maximum.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Maximum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter variable");
+        System.out.println("Enter the first variable");
         int number1 = input.nextInt();
 
-        System.out.println("Enter variable");
+        System.out.println("Enter the second variable");
         int number2 = input.nextInt();
 
-        System.out.println("Enter variable");
+        System.out.println("Enter the third variable");
         int number3 = input.nextInt();
 
-        System.out.println("The mas is");
+        System.out.println("The max is");
         if (number1 >= number2 + number3)
             System.out.println(number1);
         if (number2 >= number1 + number3)
             System.out.println(number2);
-        if (number3 >=)
+        if (number3 >= number1 + number2)
+            System.out.println(number3);
+            
+
 
     }
 }
