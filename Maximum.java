ef73a06b8367c8a570b431bca59bb61c6f401431
SiriusGuy <cs.romangodinez@gmail.com>
2025-09-11 20:09:24 +0000

    Maximum Sam

--- Maximum.java
+++ Maximum.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/**
+ * This program finds the maximum of a set of three numbers input by the user.
+ */
+
 public class Maximum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -13,11 +17,11 @@ public class Maximum {
         int number3 = input.nextInt();
 
         System.out.println("The max is");
-        if (number1 >= number2 + number3)
+        if (number1 >= number2 && number1 >= number3)
             System.out.println(number1);
-        if (number2 >= number1 + number3)
+        else if (number2 >= number1 && number2 >= number3)
             System.out.println(number2);
-        if (number3 >= number1 + number2)
+        else if (number3 >= number1 && number3 >= number2)
             System.out.println(number3);
             
 
